72554dc1ddb68f383bb811e6dffc5722e9bd67e1
Daniel Thompson <danielt3131@gmail.com>
2023-12-06 14:34:19 -0500

    Added reading in the number of categories.

--- main.c
+++ main.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
+
+int main(int argc, char *argv[]) {
+    FILE *gradeWeight = NULL;
+    FILE *gradeBook = NULL;
+    if (argc > 2){
+        gradeWeight = fopen(argv[1], "r");
+        gradeBook = fopen(argv[2], "r");
+    } else {
+        fprintf(stderr, "Unable to find files, pass in the file name as cli arguments in the order (grade weight) and grade book.\n");
+        return 0;
+    }
+    int numberOfCategories;
+    fscanf(gradeWeight, "%d", &numberOfCategories);
+    /*
+     * categorySizes stores the number of entries for each category
+     */
+    int *categorySizes = (int *) malloc(numberOfCategories * sizeof(int));
 }
