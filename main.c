f139ffd9b5fb6e77a53de899c46d20e489f2375e
Daniel Thompson <danielt3131@gmail.com>
2023-12-06 14:53:03 -0500

    Start of reading in the gradebook

--- main.c
+++ main.c
@@ -18,17 +18,27 @@ int main(int argc, char *argv[]) {
     fscanf(gradeWeight, "%d", &numberOfCategories);
     /*
      * categorySizes stores the number of entries for each category
+     * Allocating memory
      */
     int *categorySizes = (int *) malloc(numberOfCategories * sizeof(int));
     double *gradeWgt = (double *) malloc(numberOfCategories * sizeof(double));
-    char **gradeStr = (char **) malloc(numberOfCategories * sizeof(char *));
+    char **gradeWgtStr = (char *) malloc (numberOfCategories * sizeof(char *));
+    char **gradeBookStr = (char **) malloc(numberOfCategories * sizeof(char *));
+    double **grades = (double **) malloc(numberOfCategories * sizeof(double));
     for (int i = 0; i < numberOfCategories; i++){
-        gradeStr[i] = (char *) malloc(gradeStringSize * sizeof(char));
+        gradeWgtStr[i] = (char *) malloc(gradeStringSize * sizeof(char));
+        gradeBookStr[i] = (char *) malloc(gradeStringSize * sizeof(char));
+    }
+    // Read in the grade weights with the associated string
+    for (int i = 0; i < numberOfCategories; i++){
+        fscanf(gradeWeight, "%50s:%lf%%\n", (gradeWgtStr + i), (gradeWgt + i));
     }
     int readCounter = 0;
-    // Read until end of file
+    // Read until end of fil
     while (!feof(gradeBook)){
         fscanf(gradeBook, "%d", (categorySizes + readCounter));
+        grades[readCounter] = malloc(categorySizes[readCounter] * sizeof(double));
+        fscanf(gradeBook, "%50s", ())
         readCounter++;
     }
 }
